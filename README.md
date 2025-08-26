5a64edbe19cdbe5da8a11e314c05d3f4b20e01d0
Scientificat <51430248+JY-Mar@users.noreply.github.com>
2025-08-26 09:42:46 +0000

    1.0.0-0

--- README.md
+++ README.md
@@ -1,24 +1,34 @@
 A plugin for rollup or vite to compress the bundle directory which supports `.zip` `.tar` `.tgz` formats.
 
+> Forked from: https://github.com/lourain/rollup-plugin-compress-dist
+> And referred to: https://gitee.com/codercjx/rollup-plugin-compression
+
 ## Usage
 
   1. download from npm
   ```
-    npm install rollup-plugin-compress-dist --dev
+    npm install rollup-plugin-compressor --dev
   ```
   2. Modify configuration file of project. it would archive `dist` directory to `dist.tar.gz` by default.For example: 
   ```javascript
   //vite.config.ts
   import { defineConfig } from 'vite';
-  import compressDist, { CompressOptions } from 'rollup-plugin-compress-dist';
+  import compressDist, { CompressOptions } from 'rollup-plugin-compressor';
   ...
-   const compressOpts: CompressOptions<'tgz'> = {
+   const compressOpts: CompressOptions = {
      type: 'tgz',
-     archiverName: 'dist.tar.gz',
+     // The extname of targetName will not No longer necessary
+     // If the extname not exactly corresponds to the type, extname will up to the type
+     // Example: type = 'tgz', targetName = 'dist.tar.gz' => result = 'dist.tar.gz'
+     // Example: type = 'tgz', targetName = 'dist.zip' => result = 'dist.zip.tar.gz'
+     // Example: type = 'tgz', targetName = 'dist' => result = 'dist.tar.gz'
+     targetName: 'dist.tar.gz',
      sourceName: 'dist',
+     // Default package source folder, and only package the internal files if ignoreBase is true
+     ignoreBase: false
    };
   export default defineConfig({
-    plugins: [compressDist(compressOpts)],
+    plugins: [compressor(compressOptions)],
     ...
   });
   
--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,143 @@
-// import { Plugin } from "rollup";
-import { cwd } from "process";
-import { resolve } from "path";
-import compressing from "compressing";
-import chalk from "chalk";
-import { createWriteStream } from "fs";
+// Forked from: https://github.com/lourain/rollup-plugin-compress-dist
+// And referred to: https://gitee.com/codercjx/rollup-plugin-compression
 
-export interface CompressOptions<Type extends "zip" | "tar" | "tgz"> {
-  archiverName?: ArchiverName<Type>;
-  type: Type;
-  sourceName?: string;
+import { cwd } from 'process'
+import path from 'path'
+import compressing from 'compressing'
+import chalk from 'chalk'
+import fs from 'fs'
+
+enum ExtnameType {
+  zip = 'zip',
+  tar = 'tar',
+  tgz = 'tar.gz'
 }
-type ArchiverName<T> = T extends "zip" | "tar"
-  ? `${string}.${T}`
-  : T extends "tgz"
-  ? `${string}.tar.gz`
-  : never;
+const ExtnameTypeList = Object.keys(ExtnameType) as (keyof typeof ExtnameType)[]
+type CompressType = keyof typeof ExtnameType
 
-const initOpts: CompressOptions<"tgz"> = {
-  archiverName: "dist.tar.gz",
-  type: "tgz",
-  sourceName: "dist",
-};
-export default function compressDist(
-  opts?: CompressOptions<"zip" | "tar" | "tgz">
-) {
-  const { sourceName, archiverName, type } = opts || initOpts;
+export interface CompressOptions<Type extends CompressType = CompressType> {
+  sourceName?: string
+  type: Type
+  targetName?: TargetName<Type>
+  /**
+   * 默认打包源文件夹本身,配置为 true 则只打包文件夹内文件
+   */
+  ignoreBase?: boolean
+}
+type TargetName<T> = T extends 'zip' | 'tar' ? string | `${string}.${T}` : T extends 'tgz' ? string | `${string}.tar.gz` : never
+
+const defaultOption: CompressOptions<'tgz'> = {
+  type: 'tgz',
+  sourceName: 'dist',
+  targetName: 'dist.tar.gz',
+  ignoreBase: false
+}
+
+/**
+ * 删除 目录 及 目录中的所有 目录和文件
+ * @return       {*}
+ */
+function deleteDir(targetPath: string): void {
+  // 1. 判断 路径是否存在
+  if (!targetPath) return
+  if (!fs.existsSync(targetPath)) return
+  // 2. 获取该路径下所有目录名和文件名
+  const files = fs.readdirSync(targetPath)
+  files.forEach((file) => {
+    // 3. 拼接 目录下的目录路径 和 文件路径
+    const curPath = path.resolve(targetPath, file)
+    // 4. 判断是否 是目录
+    if (fs.statSync(curPath).isDirectory()) {
+      // 是目录  递归删除里面的 文件
+      deleteDir(curPath)
+    } else {
+      // 是文件 删除
+      fs.unlinkSync(curPath)
+    }
+  })
+  // 删除 目录
+  fs.rmdirSync(targetPath)
+}
+
+function deleteDirFile(targetPath: string, type: CompressType = defaultOption.type): void {
+  if (!targetPath) return
+  const rootPathFiles = fs.readdirSync(targetPath)
+  // console.log("获取==根路径下文件", rootPathFiles);
+  rootPathFiles.forEach((file) => {
+    const currentPath = path.resolve(targetPath, file)
+    // 判断是否是目录
+    if (!fs.statSync(currentPath).isDirectory()) {
+      // 不是目录 说明是文件
+      // 获取文件扩展名
+      const extname = path.extname(file)
+      const _type = (type === 'tgz' ? 'gz' : type) ?? defaultOption.type
+      if (extname === `.${_type}`) {
+        // 判断是否 是 打包的文件  是 就删除文件
+        fs.unlinkSync(currentPath)
+      }
+    }
+    // console.log("根路劲下目录和文件", currentPath);
+  })
+}
+function validExtAndType(targetPath: string, type: string): boolean {
+  return targetPath && type && new RegExp(`\.+\\.${ExtnameType[type]}\$`).test(targetPath)
+}
+
+export default function compressorBuild(options: CompressOptions | undefined = defaultOption) {
+  const sourceName = options?.sourceName ?? defaultOption.sourceName
+  const targetName = options?.targetName ?? defaultOption.targetName
+  let type = options?.type ?? defaultOption.type
+  type = ExtnameTypeList.indexOf(type) > -1 ? type : defaultOption.type
+  const ignoreBase = options?.ignoreBase ?? defaultOption.ignoreBase
+  const rootPath = cwd()
+  const buildPath = path.resolve(rootPath, sourceName)
+
   return {
-    name: "compress-dist",
-    closeBundle() {
-      console.log("closeBundle");
-      const rootPath = cwd();
-      const sourcePath = resolve(rootPath, sourceName);
-      chalk.bgBlue(`sourcePath: ${sourcePath}`);
-
-      const destStream = createWriteStream(resolve(rootPath, archiverName));
-      const sourceStream = new compressing[type].Stream();
-
-      destStream.on("finish", () => {
-        console.log(
-          chalk.cyan(`compress-dist:  ${sourceName} compress completed!`)
-        );
-      });
-
-      destStream.on("error", (err) => {
-        throw err;
-      });
-
-      sourceStream.addEntry(sourcePath);
-      sourceStream.pipe(destStream);
+    name: 'compressor',
+    buildStart() {
+      //1. 删除 build 构建目录
+      deleteDir(buildPath)
+      //2. 删除 目录下 指定的扩展名 文件
+      deleteDirFile(rootPath, type)
     },
-  };
+    // rollup-plugin-compression
+    closeBundle1() {
+      console.log('closeBundle')
+      compressing.zip
+        .compressDir(buildPath, `${targetName}.${type}`, { ignoreBase: ignoreBase })
+        .then((res: any) => {
+          console.log('compress completed ~')
+          // console.log(chalk.cyan(`compress-${sourceName}:  ${sourceName} compress completed!`));
+        })
+        .catch((err: any) => {
+          // console.log(chalk.cyan(`compress error ~`));
+        })
+    },
+    // rollup-plugin-compress-dist
+    closeBundle() {
+      console.log('closeBundle')
+      chalk.bgBlue(`buildPath: ${buildPath}`)
+      const extname = ExtnameType[type]
+      let basename: string
+      if (validExtAndType(targetName, type)) {
+        basename = targetName.substring(0, targetName.indexOf(`.${extname}`))
+      } else {
+        basename = targetName
+      }
+
+      const destStream = fs.createWriteStream(path.resolve(rootPath, `${basename}.${extname}`))
+      const sourceStream = new compressing[type].Stream()
+
+      destStream.on('finish', () => {
+        console.log(chalk.cyan(`compressor:  ${sourceName} compress completed!`))
+      })
+
+      destStream.on('error', (err) => {
+        throw err
+      })
+
+      sourceStream.addEntry(buildPath, { ignoreBase: ignoreBase })
+      sourceStream.pipe(destStream)
+    }
+  }
 }
